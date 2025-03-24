e74651a6926d4ba05d9046521f6806f63a74ff8f
DevDaniel21 <danielglaucia2.007@gmail.com>
2025-03-24 17:23:14 -0300

    Preparando Edicao

--- js/app.js
+++ js/app.js
@@ -25,5 +25,12 @@ alunos.forEach(aluno => {
     alunoService.add(new AlunoModel(aluno))
 });
 
-const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
-const alunoController = new AlunoController(alunoService, alunoView)
+const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'));
+const alunoController = new AlunoController(alunoService, alunoView);
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const nome = document.getElementById('first_name').value;
+    alunoController.add({ nome });
+})
+
